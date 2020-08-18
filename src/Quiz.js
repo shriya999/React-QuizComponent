fa09dfe0fcd8b9f7fb64777755e043665f6fc292
Shriya Gupta <shrigupt@microsoft.com>
2020-08-18 15:16:00 +0530

    Fixed import statement.

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 let quizData = require('./quiz_data.json');
 
-class Quiz extends React.Component{
+class Quiz extends Component{
     constructor(props){
         super(props);
         this.state = {quiz_position: 1};
@@ -10,7 +10,7 @@ class Quiz extends React.Component{
 
     render(){
         return <div>
-                    <div className="QuizQuestion"> {quizData.quiz_questions[0].instruction_text} </div> 
+                    <div className="QuizQuestion"> {quizData.quiz_questions[1].instruction_text} </div> 
                </div>;
     }
 }
