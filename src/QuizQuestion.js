ddfea712d9433f16abab773adf355bcb5cbc08ec
Shriya Gupta <shrigupt@microsoft.com>
2020-08-19 15:10:28 +0530

    Added onClick handler functions.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,7 +2,18 @@ import React, {Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{
+    handleClick(buttonText){
+      if (buttonText === this.props.quiz_question.answer){
+        this.props.showNextQuestionHandler()
+      }
+    }
+
     render(){
+      const buttonList = this.props.quiz_question.answer_options.map(
+        (answer_option, index)=> <QuizQuestionButton
+                                     clickHandler={this.handleClick.bind(this)}
+                                     key={index} 
+                                     button_text={answer_option}/>)
         return(
             <main>
             <section>
@@ -10,7 +21,7 @@ class QuizQuestion extends Component{
             </section>
             <section className="buttons">
               <ul>
-                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                  {buttonList}
               </ul>
             </section>
           </main>
