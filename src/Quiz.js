4e900713b471f9af4af4b17c6f325ad1a63e4bd5
Shriya Gupta <shrigupt@microsoft.com>
2020-08-19 15:21:09 +0530

    Reseting the quiz to an initial state.

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,12 +14,21 @@ class Quiz extends Component{
         this.setState({quiz_position: this.state.quiz_position+1})
     }
 
+    handleResetClick(){
+        this.setState({quiz_position: 1})
+    }
+
     render(){
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
         return (<div className="QuizQuestion"> 
-                    {isQuizEnd ? <QuizEnd/> : <QuizQuestion 
-                                                showNextQuestionHandler={this.showNextQuestion.bind(this)}
-                                                quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/> }
+                    {isQuizEnd ? <QuizEnd 
+                                    resetClickHandler={this.handleResetClick.bind(this)} 
+                                /> : 
+                                <QuizQuestion 
+                                    showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                                    quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                                /> 
+                    }
                </div>)
     }
 }
